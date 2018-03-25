87319a4ab6f15b21520f9f5f2f2026a4372752f9
Pavel <p-moiseenkov-iwb@mail.ru>
2018-03-25 21:39:54 +0300

    2 commit

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from my_app.models import Product, AppUser, UserInformation
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(AppUser)
+admin.site.register(UserInformation)
--- my_app/models.py
+++ my_app/models.py
@@ -9,17 +9,26 @@ import django.utils.timezone
 
 
 class Product(models.Model):
+    class Meta():
+        db_table="product"
     name = models.CharField(unique=True, max_length=200)
     weight = models.IntegerField(default=0)
     caloricity = models.IntegerField(default=0)
-    BGD = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    carbohydrate = models.IntegerField(default=0)
 
 
 class AppUser(models.Model):
+    class Meta():
+        db_table="appuser"
     name = models.CharField(max_length=200)
     product = models.ManyToManyField(Product)
 
 
 class UserInformation(models.Model):
+    class Meta():
+        db_table="useronfo"
     date = models.DateField(default=django.utils.timezone.now)
+    weight = models.IntegerField(default=0)
     appuser = models.OneToOneField(AppUser)
